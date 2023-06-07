82e05c443d758bf5c3d0a140ac3e0c6f94dffb99
Moew0024 <ngocchuong2410@gmail.com>
2023-06-07 14:27:56 +0700

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { themeSettings } from "../src/theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Layout from "./scenes/layout";
+import Products from "./scenes/products/index.jsx";
+import Customers from "./scenes/customers/index.jsx";
+import Transactions from "./scenes/transactions";
+import Geography from "./scenes/geography";
+import Overview from "./scenes/overview";
+import Daily from "./scenes/daily";
+import Monthly from "./scenes/monthly";
+import Breakdown from "./scenes/Breakdown";
+import Admins from "./scenes/Admins";
 function App() {
+  const mode = useSelector((state) => {
+    return state.global.mode;
+  });
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admins />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
